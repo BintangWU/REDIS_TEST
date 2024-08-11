0de279087ab255c8f6e4c0158c6adf2efe5dffc3
BintangWU <bpratama348@gmail.com>
2024-08-11 22:15:16 +0700

    add logger

--- myredis.py
+++ myredis.py
@@ -1,12 +1,32 @@
-import redis
+import redis, logging, time, os
 from redis.commands.json.path import Path
-from pydantic import BaseModel
 from typing import Optional, Any
 
 
 class MyRedis(redis.Redis):
     def __init__(self, host: str = 'localhost', port: int = 6379, user: Optional[str] = None, pwd: Optional[str] = None, debug: bool = False) -> None:
         super().__init__(host= host, port= port, username= user, password= pwd, decode_responses= True)
+        
+        self._time = time.strftime("%Y%m%d_%I%p")
+        self._log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs/redis_con')
+        self._log_name = f"REDIS-CON_{self._time}.log"
+        
+        if not os.path.exists(self._log_dir):
+            os.mkdir(self._log_dir)
+        
+        logging.basicConfig(
+            level= logging.INFO,
+            filename= os.path.join(self._log_dir, self._log_name),
+            format= "[%(asctime)s] %(levelname)s: %(message)s"
+        )
+        
+        self._console = logging.StreamHandler()
+        self._console.setLevel(logging.DEBUG)
+        self._console.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s"))
+        
+        self._logger = logging.getLogger("REDIS-CON")
+        self._logger.addHandler(self._console)
+        self._logger.info("Begin connection!!!")
         
         # self.redis_con = redis.Redis(host= host, port= port, username= user, password= pwd, decode_responses= True)
         self._debug = debug
@@ -18,11 +38,13 @@ class MyRedis(redis.Redis):
     
     
     # Write string data to Redis (write adn update)
+    # print(myRedis.set_string("HELO", "WORLD"))
     def set_string(self, key: str, value: str) -> bool:
         return self.set(key, value)
     
 
     # Write json data to Redis
+    # myRedis.set_json("test", value= data)
     def set_json(self, key: str, **kwargs) -> bool:
         if 'path' in kwargs:
             return self.json().set(key, Path(kwargs['path']), kwargs['value'])
@@ -42,10 +64,12 @@ class MyRedis(redis.Redis):
         except Exception as err:
             if self._debug:
                 print(err)
+            self._logger.error(err)
             return None
 
     
     # Read JSON data from Redis
+    # print(myRedis.get_json("cam2:region", '[0]["region"]'))
     def get_json(self, key: str, path: Optional[str] = None) -> Optional[Any]:
         try:             
             data = (self.json().get(key) if path == None else self.json().get(key, Path(path)))
@@ -58,16 +82,12 @@ class MyRedis(redis.Redis):
         except Exception as err:
             if self._debug:
                 print(err)
+            self._logger.error(err)
             return None
             
 
 if __name__ == "__main__":
     myRedis = MyRedis(pwd= "admin")
-    # print(myRedis.get_string("cam1:region"))
-    # print(myRedis.get_json("cam2:region"))
-    # print(myRedis.set_string("HELO", "WORLD"))
-    myRedis.pubsub().subscribe()
-    
     
     
     
